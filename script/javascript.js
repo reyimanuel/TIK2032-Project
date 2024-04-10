8a7488c1dafaf2a47f1dccf1856d6474b7872038
reyimanuel <miraclesumajow32@gmail.com>
2024-04-10 17:30:20 +0800

    new part

--- script/javascript.js
+++ script/javascript.js
@@ -1,16 +1,40 @@
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+    
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("appear");
+    }
+    else {
+      reveals[i].classList.remove("appear");
+    }
+  }
+};    
+
+/* Hamburger Menu */
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('header ul');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('active');
 });
+/* Hamburger Menu */
 
+/* Sticky Navigation Bar */
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+/* Sticky Navigation Bar */
 
-let slideIndex = 0; //Gallery
+/* Slide Show */
+let slideIndex = 0; 
 showSlides();
 
 function showSlides() {
@@ -29,3 +53,4 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+/* Slide Show */

fde4e946c2a8f0ee5b9c9047f350abfddf24d5eb
reyimanuel <miraclesumajow32@gmail.com>
2024-04-12 06:38:32 +0800

    tutor dong kak

--- script/javascript.js
+++ script/javascript.js
@@ -32,25 +32,3 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 /* Sticky Navigation Bar */
-
-/* Slide Show */
-let slideIndex = 0; 
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-/* Slide Show */
